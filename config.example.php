f8d4ec6838f8b5cd300e86f6bd1aad78c440e63d
An Hoang <an.hoang@go1.com>
2018-11-30 01:28:01 +0000

    Initial commit

---
+++ config.example.php
@@ -0,0 +1,21 @@
+<?php
+
+$key = ''; # Change these to your respective AWS credentials
+$secret = '';
+
+return [
+    'pollyOptions' => [
+        'version'     => 'latest',
+        'region'      => 'ap-southeast-1', # Change this to your respective AWS region
+        'credentials' => [
+            'key'    => $key,
+            'secret' => $secret,
+        ],
+    ],
+    's3Options'    => [
+        'version'     => 'latest',
+        'region'      => 'ap-southeast-1',
+        'credentials' => new \Aws\Credentials\Credentials($key, $secret),
+        'bucket'  => 'hackathon',
+    ],
+];
